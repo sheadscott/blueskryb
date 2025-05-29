c4bac1c9cb49b831c396debe466da9a32ae104c0
Shea Scott <sheadscott@gmail.com>
2025-05-29 12:23:07 -0500

    Signup for production

--- src/app/client-metadata.json/route.ts
+++ src/app/client-metadata.json/route.ts
@@ -1,6 +1,7 @@
 import { blueskyClientMetadata } from '@/lib/atproto'
 import { NextResponse } from 'next/server'
 
+// Comment for git
 export async function GET(): Promise<NextResponse> {
   return NextResponse.json(blueskyClientMetadata(), {
     status: 200,
